65d8127c6cb5edcc5264e1dbae0484be5c63ef0c
Arnav-gaumat <arnavgaumat2014@gmail.com>
2024-07-18 12:55:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Flipcoin from './components/flipcoin';
+import './App.css';
+
+const App=()=> {
+  return(
+    <div classname='App'>
+      <Flipcoin/>
+    </div>
+  );
+}
+export default App;
